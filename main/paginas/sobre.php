d120d4de15130c305fd39f6980e42b41fb0d5dd5
Luked616 <theluked616@gmail.com>
2024-11-21 15:46:13 -0300

    update sobre nós

--- main/paginas/sobre.php
+++ main/paginas/sobre.php
@@ -14,14 +14,42 @@ require_once("navbar.php");
 
 <body>
     <main>
-        <div class="titulo"><h1>Sobre a Diamond Drugs:</h1></div>
-        <div class="texto"><h2>Fundada por Dr. Kojima, a Diamond Drugs surgiu com um único objetivo: oferecer produtos farmacêuticos de qualidade, com a mesma dedicação e precisão que um verdadeiro profissional de saúde pode confiar. Inspirados pela visão incansável do nosso fundador, que sempre acreditou no poder de transformar a saúde das pessoas com atenção e expertise, somos um verdadeiro aliado no cuidado com o bem-estar.</h2></div>
-        <div class="img"><img src="../imagens/fundador.png" alt="fundador" class="img2"></div>
-        <div class="texto"><h2>Em nossa jornada, seguimos a filosofia de inovar constantemente, como se estivéssemos em uma missão estratégica. Assim como a evolução das tecnologias que cercam nossos heróis, mantemos um compromisso inabalável com a excelência, oferecendo medicamentos e soluções eficazes para todas as necessidades de nossos clientes. Sabemos que o cuidado com a saúde exige um planejamento detalhado, onde cada passo conta, e nós estamos sempre um passo à frente, preparados para enfrentar qualquer desafio.</h2></div>
-        <div class="img"><img src="../imagens/teste.webp" alt="experimentos" class="img2"></div>
-        <div class="texto"><h2>Na Diamond Drugs, a missão de proteger a saúde de nossos clientes é tratada com a mesma responsabilidade que um soldado em uma missão crucial. Contamos com profissionais qualificados, sempre prontos para atender com discrição, dedicação e a confiança de quem sabe o que é necessário para uma vida mais saudável. Em nosso compromisso com a precisão e excelência, seguimos os princípios de nossos fundadores: oferecer apenas o melhor, com a segurança de quem entende o valor de um compromisso vital.</h2></div>
-        <div class="img"><img src="../imagens/lab.png" alt="lab" class="img2"></div>
-        <div class="texto"><h2>Seja para uma consulta simples ou para o fornecimento de medicamentos especializados, a Diamond Drugs é a escolha certa. Com um olhar atento às necessidades do cliente e um compromisso com o futuro da saúde, estamos prontos para enfrentar qualquer missão que você nos confiar.</h2></div>
+        <div class="center">
+            <div class="titulo">
+                <h1>Sobre a Diamond Drugs:</h1>
+            </div>
+            <div class="texto">
+                <h2>Fundada por Dr. Kojima, a Diamond Drugs surgiu com um único objetivo: oferecer produtos
+                    farmacêuticos de qualidade, com a mesma dedicação e precisão que um verdadeiro profissional de saúde
+                    pode confiar. Inspirados pela visão incansável do nosso fundador, que sempre acreditou no poder de
+                    transformar a saúde das pessoas com atenção e expertise, somos um verdadeiro aliado no cuidado com o
+                    bem-estar.</h2>
+            </div>
+            <div class="img"><img src="../imagens/fundador.png" alt="fundador" class="img2"></div>
+            <div class="texto">
+                <h2>Em nossa jornada, seguimos a filosofia de inovar constantemente, como se estivéssemos em uma missão
+                    estratégica. Assim como a evolução das tecnologias que cercam nossos heróis, mantemos um compromisso
+                    inabalável com a excelência, oferecendo medicamentos e soluções eficazes para todas as necessidades
+                    de nossos clientes. Sabemos que o cuidado com a saúde exige um planejamento detalhado, onde cada
+                    passo conta, e nós estamos sempre um passo à frente, preparados para enfrentar qualquer desafio.
+                </h2>
+            </div>
+            <div class="img"><img src="../imagens/teste.webp" alt="experimentos" class="img2"></div>
+            <div class="texto">
+                <h2>Na Diamond Drugs, a missão de proteger a saúde de nossos clientes é tratada com a mesma
+                    responsabilidade que um soldado em uma missão crucial. Contamos com profissionais qualificados,
+                    sempre prontos para atender com discrição, dedicação e a confiança de quem sabe o que é necessário
+                    para uma vida mais saudável. Em nosso compromisso com a precisão e excelência, seguimos os
+                    princípios de nossos fundadores: oferecer apenas o melhor, com a segurança de quem entende o valor
+                    de um compromisso vital.</h2>
+            </div>
+            <div class="img"><img src="../imagens/lab.png" alt="lab" class="img2"></div>
+            <div class="texto">
+                <h2>Seja para uma consulta simples ou para o fornecimento de medicamentos especializados, a Diamond
+                    Drugs é a escolha certa. Com um olhar atento às necessidades do cliente e um compromisso com o
+                    futuro da saúde, estamos prontos para enfrentar qualquer missão que você nos confiar.</h2>
+            </div>
+        </div>
     </main>
 </body>
 
